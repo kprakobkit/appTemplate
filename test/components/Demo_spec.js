56ce48969905741bbbaa40ef13dd8a56b9180a40
achiu8 <achiu8@gmail.com>
2015-10-23 11:09:05 -0700

    Add passing test for rendered component

--- test/components/Demo_spec.js
+++ test/components/Demo_spec.js
@@ -4,10 +4,21 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import Demo from '../../app/components/Demo';
 
+const { renderIntoDocument } = TestUtils;
 const demo = React.createFactory(Demo);
 
 describe('Demo', () => {
   it('passes dummy test', () => {
     expect(true).to.equal(true);
+  });
+
+  it('renders successfully with class name and message', () => {
+    const message = 'hello there';
+    const component = renderIntoDocument(demo({ message: message }));
+    const rendered = ReactDOM.findDOMNode(component);
+
+    expect(rendered).to.be.ok;
+    expect(rendered.className).to.contain('demo');
+    expect(rendered.textContent).to.equal(message);
   });
 });
